99102d4e0cacc1c11c730e8f79ab6e15d6cb23cc
Duchicus <145117344+Duchicus@users.noreply.github.com>
2024-02-27 17:50:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Js-2-1-1-
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Duchicus/Js-2-1-1-)
---
+++ script.js
@@ -0,0 +1,11 @@
+// Declara tres variables utilizando let o const con diferentes tipos de datos: un número, una cadena de caracteres y un booleano.
+
+let numero = 1;
+let cadena = 'hola';
+let boolean = true;
+
+// Muestra en la consola el contenido de cada variable utilizando console.log().
+
+console.log(boolean, numero, cadena);
+
+// Intenta hacer cambios en cada una de las variables con otros valores y tipos
